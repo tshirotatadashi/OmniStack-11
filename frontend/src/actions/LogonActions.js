933c3991a48b03af3495577bec0b0dfd426d13c1
tshirotatadashi <tshirotatadashi@gmail.com>
2020-03-29 11:43:00 -0400

    feat(Logon): integration with API implemented

---
+++ frontend/src/actions/LogonActions.js
@@ -0,0 +1,15 @@
+import api from "../services/api";
+
+export default async function store(payload) {
+  return new Promise ((resolve, reject) =>{
+    api.post("sessions", payload).then(({ data }) => {
+        const { id, name } = data
+        localStorage.setItem('ongId', id)
+        localStorage.setItem('ongName', name)
+        resolve()
+      }).catch(error => {
+        console.error(error)
+        reject()
+      })
+  })
+}
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import logon from '../../actions/LogonActions'
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 import "./style.css";
 import logoImg from "../../assets/logo.svg";
 import herosImg from "../../assets/heros.png";
 
 export default function Logon() {
+  const [id, setId] = useState('')
+  const history = useHistory()
+
+  function hadleLogin(event) {
+    event.preventDefault();
+    logon({ id })
+      .then(() => {
+        history.push('/profile')
+      }).catch(() => {
+        alert('Erro ao fazer o login')
+      })
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={hadleLogin}>
           <h1>Faça seu Logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
           <button type="submit" className="button">
             Entrar
           </button>
