9b88e764f26568cbcb2c2938e9fd0bea89ef61c0
tshirotatadashi <tshirotatadashi@gmail.com>
2020-03-29 13:39:02 -0400

    fix(ProfileControler): fixed authorization variable

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,7 +2,7 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(request, response) {
-    const ong_id = request.headers.authorizarion;
+    const ong_id = request.headers.authorization;
     const incidents = await connection("incidents")
       .where("ong_id", ong_id)
       .select("*");
